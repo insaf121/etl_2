635d25099e10fb564e8cd699246f1f823fdb2d53
insaf121 <insaf121@gmail.com>
2025-07-19 19:42:05 +0300

    fix 2.3

--- src/2.3.sql
+++ src/2.3.sql
@@ -1,88 +1,37 @@
-WITH corrected_in_sum AS (
-    SELECT 
-        ab1.account_rk,
-        ab1.effective_date,
-        CASE 
-            WHEN ab1.account_in_sum != LAG(ab2.account_out_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date) 
-            THEN LAG(ab2.account_out_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date)
-            ELSE ab1.account_in_sum
-        END AS corrected_account_in_sum
-    FROM 
-        rd.account_balance ab1
-    JOIN 
-        rd.account_balance ab2 ON ab1.account_rk = ab2.account_rk 
-                              AND ab1.effective_date = ab2.effective_date + INTERVAL '1 day'
-)
-SELECT * FROM corrected_in_sum;
-
-
-WITH corrected_out_sum AS (
-    SELECT 
-        ab1.account_rk,
-        ab1.effective_date,
-        CASE 
-            WHEN ab1.account_out_sum != LEAD(ab2.account_in_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date) 
-            THEN LEAD(ab2.account_in_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date)
-            ELSE ab1.account_out_sum
-        END AS corrected_account_out_sum
-    FROM 
-        rd.account_balance ab1
-    JOIN 
-        rd.account_balance ab2 ON ab1.account_rk = ab2.account_rk 
-                              AND ab1.effective_date = ab2.effective_date - INTERVAL '1 day'
-)
-SELECT * FROM corrected_out_sum;
-
-
-UPDATE rd.account_balance ab
-SET account_in_sum = corrected.corrected_account_in_sum
-FROM (
-    SELECT 
-        ab1.account_rk,
-        ab1.effective_date,
-        CASE 
-            WHEN ab1.account_in_sum != LAG(ab2.account_out_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date) 
-            THEN LAG(ab2.account_out_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date)
-            ELSE ab1.account_in_sum
-        END AS corrected_account_in_sum
-    FROM 
-        rd.account_balance ab1
-    JOIN 
-        rd.account_balance ab2 ON ab1.account_rk = ab2.account_rk 
-                              AND ab1.effective_date = ab2.effective_date + INTERVAL '1 day'
-) corrected
-WHERE ab.account_rk = corrected.account_rk 
-  AND ab.effective_date = corrected.effective_date 
-  AND ab.account_in_sum != corrected.corrected_account_in_sum;
-
-
-
 CREATE OR REPLACE PROCEDURE reload_account_balance_turnover()
-LANGUAGE SQL
+LANGUAGE plpgsql
 AS $$
-    -- Обновляем некорректные account_out_sum (по правилу из п.2)
-    UPDATE rd.account_balance ab
-    SET account_out_sum = corrected.corrected_account_out_sum
-    FROM (
+DECLARE
+    rows_updated INTEGER;
+    rows_inserted INTEGER;
+    start_time TIMESTAMP := clock_timestamp();
+BEGIN
+    -- Логирование начала процедуры
+    RAISE NOTICE 'Начало процедуры перезагрузки витрины dm.account_balance_turnover: %', start_time;
+    
+    -- 1. Корректировка некорректных account_out_sum
+    WITH corrected_data AS (
         SELECT 
             ab1.account_rk,
             ab1.effective_date,
-            CASE 
-                WHEN ab1.account_out_sum != LEAD(ab2.account_in_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date) 
-                THEN LEAD(ab2.account_in_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date)
-                ELSE ab1.account_out_sum
-            END AS corrected_account_out_sum
+            LEAD(ab2.account_in_sum) OVER (PARTITION BY ab1.account_rk ORDER BY ab1.effective_date) AS next_day_in_sum
         FROM 
             rd.account_balance ab1
         JOIN 
             rd.account_balance ab2 ON ab1.account_rk = ab2.account_rk 
-                                    AND ab1.effective_date = ab2.effective_date - INTERVAL '1 day'
-    ) corrected
-    WHERE ab.account_rk = corrected.account_rk 
-      AND ab.effective_date = corrected.effective_date 
-      AND ab.account_out_sum != corrected.corrected_account_out_sum;
-
-    -- Перезагружаем витрину (согласно прототипу)
+                                  AND ab1.effective_date = ab2.effective_date - INTERVAL '1 day'
+    )
+    UPDATE rd.account_balance ab
+    SET account_out_sum = cd.next_day_in_sum
+    FROM corrected_data cd
+    WHERE ab.account_rk = cd.account_rk
+      AND ab.effective_date = cd.effective_date
+      AND ab.account_out_sum IS DISTINCT FROM cd.next_day_in_sum;
+    
+    GET DIAGNOSTICS rows_updated = ROW_COUNT;
+    RAISE NOTICE 'Обновлено % строк в rd.account_balance', rows_updated;
+    
+    -- 2. Очистка и перезагрузка витрины
     TRUNCATE TABLE dm.account_balance_turnover;
     
     INSERT INTO dm.account_balance_turnover
@@ -98,5 +47,20 @@ AS $$
     LEFT JOIN 
         rd.account_balance ab ON a.account_rk = ab.account_rk
     LEFT JOIN 
-        dm.dict_currency dc ON a.currency_cd = dc.currency_cd;
+        dm.dict_currency dc ON a.currency_cd = dc.currency_cd
+    WHERE 
+        ab.effective_date IS NOT NULL;
+    
+    GET DIAGNOSTICS rows_inserted = ROW_COUNT;
+    
+    -- Логирование завершения
+    RAISE NOTICE 'Перезагрузка завершена. Вставлено % строк. Время выполнения: %', 
+        rows_inserted, 
+        clock_timestamp() - start_time;
+EXCEPTION
+    WHEN OTHERS THEN
+        RAISE EXCEPTION 'Ошибка при перезагрузке витрины: %', SQLERRM;
+        -- Откат транзакции при ошибке
+        ROLLBACK;
+END;
 $$;
